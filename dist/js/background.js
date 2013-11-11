6065ccb4ad03dded48c3a1a38eeafa788843da25
Lihan Li <frankieteardrop@gmail.com>
2013-11-11 18:07:31 -0500

    open options page on clicking icon

--- dist/js/background.js
+++ dist/js/background.js
@@ -32,4 +32,28 @@
     return _results;
   });
 
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    var optionsUrl;
+    optionsUrl = chrome.extension.getURL("dist/options/index.html");
+    return chrome.tabs.query({}, function(extensionTabs) {
+      var found, i;
+      found = false;
+      i = 0;
+      while (i < extensionTabs.length) {
+        if (optionsUrl === extensionTabs[i].url) {
+          found = true;
+          chrome.tabs.update(extensionTabs[i].id, {
+            selected: true
+          });
+        }
+        i++;
+      }
+      if (!found) {
+        return chrome.tabs.create({
+          url: optionsUrl
+        });
+      }
+    });
+  });
+
 }).call(this);
--- src/js/background.coffee
+++ src/js/background.coffee
@@ -13,3 +13,18 @@ chrome.extension.onMessage.addListener (req, __, sendResponse) ->
 
   for k,v of req
     settings[k] v
+
+chrome.browserAction.onClicked.addListener (tab) ->
+  optionsUrl = chrome.extension.getURL("dist/options/index.html")
+  chrome.tabs.query {}, (extensionTabs) ->
+    found = false
+    i = 0
+
+    while i < extensionTabs.length
+      if optionsUrl is extensionTabs[i].url
+        found = true
+        chrome.tabs.update extensionTabs[i].id,
+          selected: true
+
+      i++
+    chrome.tabs.create url: optionsUrl unless found
